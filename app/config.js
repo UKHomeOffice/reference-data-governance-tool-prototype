182ad0da40d8302d9aa331f13a48ba82dc052cc7
T0MT33 <tom@seven-twenty.co.uk>
2019-03-11 18:00:46 +0000

    Split search function and general UI updates

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Reference Data Service',
+  serviceName: 'Reference Data Governance Tool',
 
   // Default port that prototype runs on
   port: '3000',
